0a70fd0ec8a7530ba66254bd1f4bb764ecef41b1
Robert Saunders <robertwsaunders@outlook.com>
2017-01-07 16:41:48 -0500

    Clean Up

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,25 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "pages#dashboard"
-  resources :profile, :controller => 'users'
-resources :job_postings do
-  member do
-    get 'approve'
-    get 'withdraw'
-  end
-  resources :job_applications
-end
-  resources :jobs
 
+  # devise routes for authentication
+  devise_for :users
+
+  # define the index route
+  root to: "pages#dashboard"
+
+  # define the profile routes, linked to the users controller
+  resources :profile, :controller => 'users'
+
+  # define the jobs resources
+  resources :jobs do
+    # within the jobs routes, route to job applications
+    resources :job_applications
+    end
+  end
+
+  # route to job postings
+  resources :job_postings do
+    member do
+      get 'approve'
+      get 'withdraw'
+    end
 end
