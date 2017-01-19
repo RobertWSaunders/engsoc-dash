8e5ad76ad0c6ae5f0afb6bc8155eeda51d02fb95
Peter <peterju35@gmail.com>
2017-01-18 22:02:40 -0500

    adding seed files for different users and roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(first_name:  "Peter",
+             last_name:   "SuperAdmin",
+             email:       "superadmin@example",
+             password:    "password",
+             role:        "superadmin")
+
+User.create!(first_name:  "Peter",
+             last_name:   "Admin",
+             email:       "admin@example",
+             password:    "password",
+             role:        "admin")
+
+User.create!(first_name:  "Peter",
+             last_name:   "Manager",
+             email:       "management@example",
+             password:    "password",
+             role:        "management")
+
+User.create!(first_name:  "Peter",
+             last_name:   "student",
+             email:       "student@example",
+             password:    "password",
+             role:        0)
+
+User.create!(first_name:  "Paisley",
+             last_name:   "student2",
+             email:       "student2@example",
+             password:    "password",
+             role:        0)
