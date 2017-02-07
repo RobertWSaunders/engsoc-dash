d50b27db571c5e94c1b2b0c040e18df20500d2ee
Peter <peterju35@gmail.com>
2017-02-07 18:35:42 -0500

    add validation checks to job postings model

--- app/models/job_posting.rb
+++ app/models/job_posting.rb
@@ -4,6 +4,6 @@ class JobPosting < ApplicationRecord
   has_many :job_applications, dependent: :destroy
   enum status: [:waiting_approval, :draft, :open, :interviews_pending, :interviews_scheduled, :closed ]
   validates :organization_id, presence: true
-  validates :title, prsence: true, length: { minimum: 5, maximum: 100 }
+  validates :title, presence: true, length: { minimum: 5, maximum: 100 }
   validates :description, presence: true, length: { minimum: 15, maximum: 2000 }
 end
