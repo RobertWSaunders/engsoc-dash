d454b910b390a4dcb0b271312d7a162ee59b4151
Robert Saunders <robertwsaunders@outlook.com>
2017-03-13 21:37:11 -0400

    Commenting, added extension_pending status to job posting

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,8 +1,12 @@
 class Job < ApplicationRecord
+  #Relationships
   belongs_to :organization
   belongs_to :user
+
+  #Validations
   validates :organization_id, presence: true
   validates :user_id, presence: true, :uniqueness => { :scope => :organization_id }
   validates :title, presence: true
+
   accepts_nested_attributes_for :user, :organization
 end
