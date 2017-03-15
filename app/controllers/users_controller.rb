7422f510f2ff3e2891db3184a4a4cc4d8c9a4db0
Peter <peterju35@gmail.com>
2017-03-15 00:20:30 -0400

    add edit and update routes (need to modify edit page now)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,23 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to controller: 'user', action: 'show', id: @user.id
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :description, :email)
+    end
+
 end
