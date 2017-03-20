92792d1ba92221f5eb9d4586d388008a1c406902
Robert Saunders <robertwsaunders@outlook.com>
2017-03-20 19:26:49 -0400

    Added organization status label

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
-  # content for the status labels
+
+  # content for the job posting status labels
   def status_label(posting)
     case posting.status
       when "waiting_approval"
@@ -14,6 +15,18 @@ module ApplicationHelper
         content_tag(:div, 'interviews scheduled', class: 'label label-info')
       else
         content_tag(:div, 'closed', class: 'label label-danger')
+    end
+  end
+
+  # content for the organization status labels
+  def status_label(organization)
+    case organization.status
+      when "waiting_approval"
+        content_tag(:div, 'pending approval', class: 'label label-warning')
+      when "active"
+        content_tag(:div, 'active', class: 'label label-success')
+      else
+        content_tag(:div, 'archived', class: 'label label-danger')
     end
   end
 
