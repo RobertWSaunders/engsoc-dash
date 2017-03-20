ff6c0c5e369d1921b0b68e5c5408df31803d209e
Robert Saunders <robertwsaunders@outlook.com>
2017-03-19 23:15:02 -0400

    Removed devise email config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,30 +22,9 @@ Rails.application.configure do
     }
   else
     config.action_controller.perform_caching = false
- 
+
     config.cache_store = :null_store
   end
-
-
-  config.action_mailer.default_url_options = { host: '137.74.175.51', port: 3001 }
-
-  config.action_mailer.perform_caching = false
-
-  config.action_mailer.perform_deliveries = true
-
-  config.action_mailer.raise_delivery_errors = true
-
-  config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_settings = {
-    user_name:      Rails.application.secrets.mail_username,
-    password:       Rails.application.secrets.mail_password,
-    domain:         'gmail.com',
-    address:       'smtp.gmail.com',
-    port:          '587',
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
