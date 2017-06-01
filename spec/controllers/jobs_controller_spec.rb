fa067bfe33e1d7a4b2700ce83a5d98b20895defa
Peter <peterju35@gmail.com>
2017-06-02 00:10:59 +0900

    begin work on job_controller spec, added an intentionally failing test case of an inactive org's job shouldn't be visible to student users (just superadmin)

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -31,16 +31,34 @@ describe JobsController do
         expect(response).to redirect_to(organization_path(:id => organization.id))
       end
     end
+
+    describe "GET #show for an inactive organization job" do
+      it "should render the show view" do
+        organization = create(:organization, :waiting_approval)
+        job = create(:job, organization: organization)
+        get :show, params: { id: organization.jobs.first }
+        expect(response).to render_template :show
+      end
+    end
   end
 
   context "When logged in as student" do
     login_student
 
-    describe "GET #show" do
+    describe "GET #show for an active organization job" do
       it "renders the show view" do
         organization = create(:organization_with_regular_job)
         get :show, params: { id: organization.jobs.first }
         expect(response).to render_template :show
+      end
+    end
+
+    describe "GET #show for an inactive organization job" do
+      it "shouldn't render the show view" do
+        organization = create(:organization, :waiting_approval)
+        job = create(:job, organization: organization)
+        get :show, params: { id: organization.jobs.first }
+        expect(response).not_to render_template :show
       end
     end
   end
