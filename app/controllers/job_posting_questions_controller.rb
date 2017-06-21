f297a1ba8f995b4ae8180e75b232d2575891ff6f
Peter <peterju35@gmail.com>
2017-06-21 20:34:12 +0900

    jpqs can only be modified if posting is waiting approval

--- app/controllers/job_posting_questions_controller.rb
+++ app/controllers/job_posting_questions_controller.rb
@@ -11,32 +11,48 @@ class JobPostingQuestionsController < ApplicationController
 
   # POST /job_postings/:job_posting_id/job_posting_questions
   def create
-    @jobpostingquestion = JobPostingQuestion.new(job_posting_question_params)
-    if @jobpostingquestion.save
-      redirect_to job_posting_job_posting_questions_path
+    if @jobposting.status != "waiting_approval"
+      flash[:danger] = "The Job Posting is currently not 'waiting approval', so questions cannot be created, deleted, or editted. Please contact an administrator if you require assistance."
+      redirect_to :back
     else
-      render 'index'
+      @jobpostingquestion = JobPostingQuestion.new(job_posting_question_params)
+      if @jobpostingquestion.save
+        redirect_to job_posting_job_posting_questions_path
+      else
+        flash[:danger] = "The Job Posting question was not saved. Please try again, or contact an administrator."
+        redirect_to :back
+      end
     end
   end
 
   # UPDATE /job_postings/:job_posting_id/job_posting_questions
   def update
-    question = JobPostingQuestion.find(job_posting_question_params[:id])
-    if question.update_attributes(job_posting_question_params)
-      flash[:success] = "Job Posting Question Successfully Updated"
-      redirect_to job_posting_job_posting_questions_path(:job_posting_id => @questions.first.job_posting.id)
+    if @jobposting.status != "waiting_approval"
+      flash[:danger] = "The Job Posting is currently not 'waiting approval', so questions cannot be created, deleted, or editted. Please contact an administrator if you require assistance."
+      redirect_to :back
     else
-      flash[:danger] = "Job Posting Question not updated (question too short, must be at least 5 characters)"
-      redirect_to job_posting_job_posting_questions_path(:job_posting_id => @questions.first.job_posting.id)
+      question = JobPostingQuestion.find(job_posting_question_params[:id])
+      if question.update_attributes(job_posting_question_params)
+        flash[:success] = "Job Posting Question Successfully Updated"
+        redirect_to job_posting_job_posting_questions_path(:job_posting_id => @questions.first.job_posting.id)
+      else
+        flash[:danger] = "Job Posting Question not updated (question too short, must be at least 5 characters)"
+        redirect_to job_posting_job_posting_questions_path(:job_posting_id => @questions.first.job_posting.id)
+      end
     end
   end
 
   # DESTROY /job_posting_questions/:id
   def destroy
-    @question = JobPostingQuestion.find(params[:id])
-    @question.destroy
-    flash[:success] = "Job Posting Question Successfully Deleted!"
-    redirect_to job_posting_job_posting_questions_path(:job_posting_id => @question.job_posting.id)
+    if @jobposting.status != "waiting_approval"
+      flash[:danger] = "The Job Posting is currently not 'waiting approval', so questions cannot be created, deleted, or editted. Please contact an administrator if you require assistance."
+      redirect_to :back
+    else
+      @question = JobPostingQuestion.find(params[:id])
+      @question.destroy
+      flash[:success] = "Job Posting Question Successfully Deleted!"
+      redirect_to job_posting_job_posting_questions_path(:job_posting_id => @question.job_posting.id)
+    end
   end
 
 
