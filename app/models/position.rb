72d67f471664df54caf62df17292624c6bfcae99
Peter <peterju35@gmail.com>
2017-07-13 21:37:10 -0400

    only 1 user per job, touch up job show view

--- app/models/position.rb
+++ app/models/position.rb
@@ -4,7 +4,7 @@ class Position < ApplicationRecord
   belongs_to :user
 
   validates :job_id, presence: true
-  validates :user_id, presence: true
+  validates :user_id, presence: true, :uniqueness => {:scope => :job_id}
   validates :start_date, presence: true
   validates :end_date, presence: true
 
