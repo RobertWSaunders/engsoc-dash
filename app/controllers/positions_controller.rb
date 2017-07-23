f1ec8425cca2c6c5224bc5e0356a3b03a7740e53
Peter <peterju35@gmail.com>
2017-07-23 19:58:48 -0400

    finish implementing admin position editting and deleting

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -2,8 +2,35 @@ class PositionsController < ApplicationController
 
   # GET /positions/admin
   def admin
-  @organizations = Organization.all
+    @organizations = Organization.all
     @positions = Position.all.joins(:job).order("jobs.organization_id").reverse_order
   end
 
+  # PUT /positions/edit
+  def update
+    position = Position.find(position_params[:id])
+    if position.update_attributes(position_params)
+      flash[:success] = "Position " + position.job.title + " held by " + position.user.first_name + " " + position.user.last_name + " has been updated."
+      redirect_to admin_positions_path
+    else
+      flash.keep[:danger] = "Could not update position " + position.job.title + " held by " + position.user.first_name + " " + position.user.last_name + "."
+      flash[:danger] << "<li>" + position.errors.full_messages.join('</li><li>')
+      flash[:danger] << "</ul>"
+      redirect_to admin_positions_path
+    end
+  end
+
+  # DESTROY /positions/:id
+  def destroy
+    @position = Position.find(params[:id])
+    @position.destroy
+    flash[:success] = "Position " + @position.job.title + " held by " + @position.user.first_name + " " + @position.user.last_name + " has been successfully deleted."
+    redirect_to admin_positions_path
+  end
+
+  private
+
+    def position_params
+      params.require(:position).permit(:start_date, :end_date, :id)
+    end
 end
