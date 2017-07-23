afc06999fbc8ddbc95d12a3fa229b39975ddbd7d
Peter <peterju35@gmail.com>
2017-07-23 01:04:39 -0400

    display positions by jobs, by organizations for admin positions view, add sidemenu

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -2,6 +2,7 @@ class PositionsController < ApplicationController
 
   # GET /positions/admin
   def admin
+  @organizations = Organization.all
     @positions = Position.all.joins(:job).order("jobs.organization_id").reverse_order
   end
 
