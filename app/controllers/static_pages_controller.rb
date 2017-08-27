7b0ae28479cf3ee4d33322955d244cbdd8b1a479
Peter <peterju35@gmail.com>
2017-08-27 17:17:44 -0400

    frontend work on job postings

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    @job_postings = JobPosting.where(status: "open").order(:deadline).limit(3)
+    @job_postings = JobPosting.where(status: "open").order(:updated_at).reverse_order.limit(3)
     @job_applications = JobApplication.where(user_id: current_user.id, status: ["submitted", "draft"]).order(:updated_at).reverse_order.limit(5)
     @job_interviews = Interview.includes(:job_application).where(:job_applications => { :user_id => current_user.id, :status => "interview_scheduled" }).limit(5)
   end
