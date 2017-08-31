bb182602d92a03ef15b6ec5735c21a216bffade7
Peter <peterju35@gmail.com>
2017-08-30 21:35:49 -0400

    disable mailer for now

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,25 +3,27 @@ class UserMailer < ApplicationMailer
   def welcome(user)
     @user = user
     @url = root_url
-    mail(:to => @user.email, :subject => "Welcome to Dash")
+    # mail(:to => @user.email, :subject => "Welcome to Dash")
   end
 
   def submit_job_application(job_application)
     @job_application = job_application
     @user = @job_application.user
-    mail(:to => @user.email, :subject => "Job Application for " + @job_application.job_posting.title)
+    # mail(:to => @user.email, :subject => "Job Application for " + @job_application.job_posting.title)
   end
 
   def hire_job_application(job_application)
     @job_application = job_application
     @user = @job_application.user
-    mail(:to => @user.email, :subject => "Congratulations! You're now a " + @job_application.job_posting.title)
+    # mail(:to => @user.email, :subject => "Congratulations! You're now a " + @job_application.job_posting.title)
   end
 
   def decline_job_application(job_application)
     @job_application = job_application
     @user = @job_application.user
-    mail(:to => @user.email, :subject => "Regarding your Job Application for " + @job_application.job_posting.title)
+    # mail(:to => @user.email, :subject => "Regarding your Job Application for " + @job_application.job_posting.title)
   end
+
+  # commentout mail to prevent breaking on heroku for now
 
 end
