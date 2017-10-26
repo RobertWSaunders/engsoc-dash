de09cf468105e436a95d4dec9990cc7c36100f91
Peter <peterju35@gmail.com>
2017-10-26 19:55:15 -0400

    shibboleth testing on home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
+    # p request.headers
+    @email = request.headers["email"]
+    @headers = request.headers
     @job_postings = JobPosting.where(status: "open").order(:updated_at).reverse_order.limit(3)
     @job_applications = JobApplication.where(user_id: current_user.id, status: ["submitted", "draft"]).order(:updated_at).reverse_order.limit(5)
     @job_interviews = Interview.includes(:job_application).where(:job_applications => { :user_id => current_user.id, :status => "interview_scheduled" }).limit(5)
