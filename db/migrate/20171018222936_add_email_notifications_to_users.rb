8173cd7080b6fa2f0e0c6dae577c79486ba0ad29
Peter <peterju35@gmail.com>
2017-10-26 19:57:41 -0400

    first minor fix for migration

--- db/migrate/20171018222936_add_email_notifications_to_users.rb
+++ db/migrate/20171018222936_add_email_notifications_to_users.rb
@@ -1,4 +1,4 @@
-class Addemail_notificationsToUsers < ActiveRecord::Migration[5.0]
+class AddEmailNotificationsToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :email_notifications, :boolean, default: true
   end
