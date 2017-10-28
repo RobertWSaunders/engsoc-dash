fa81d561e86c502c9de07e224700f891ff04a0eb
Peter <peterju35@gmail.com>
2017-10-28 13:59:20 -0400

    remove pw constraint

---
+++ db/migrate/20171028175738_remove_pass_constraint_from_users.rb
@@ -0,0 +1,5 @@
+class RemovePassConstraintFromUsers < ActiveRecord::Migration[5.0]
+  def change
+    change_column_null :users, :encrypted_password, true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020230936) do
+ActiveRecord::Schema.define(version: 20171028175738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,7 +114,7 @@ ActiveRecord::Schema.define(version: 20171020230936) do
     t.string   "last_name"
     t.string   "phone_number"
     t.string   "email",                  default: "",   null: false
-    t.string   "encrypted_password",     default: "",   null: false
+    t.string   "encrypted_password",     default: ""
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
