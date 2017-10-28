6d13ececc915b4bd1076ef3e69d3a64e4bf4808b
Peter <peterju35@gmail.com>
2017-10-28 13:25:23 -0400

    turn on errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Disable serving static files from the `/public` folder by default since
@@ -25,7 +25,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
   config.serve_static_assets = true
-  
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
