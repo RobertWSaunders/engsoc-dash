24c54b41fcb75cb332f9bb2007d3ef58507057c0
Max Karan <32376872+maxkaran@users.noreply.github.com>
2018-09-21 14:48:17 -0400

    Update production.rb
    The gmail credentials conflicted with actual sendgrid credentials, causing problems with sending emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,14 +89,6 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
-   :authentication       => "plain",
-  :enable_starttls_auto => true
-  }
   config.action_mailer.default_url_options = {
     host: 'dash.engsoc.queensu.ca', protocol: 'https://'
   }
